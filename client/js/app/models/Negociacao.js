ca64d1c3276b290ca6b90afc21a19690bf5c2f0e
williamgundim <williammatos09@gmail.com>
2019-08-22 06:51:50 -0300

    commit

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,31 @@
+
+class Negociacao{
+
+    constructor(date, quant, valor) {
+
+        this._data = date;
+        this._quantidade = quant;
+        this._valor = valor;
+        Object.freeze(this);
+    }
+
+    get volume() {
+        return this._quantidade * this.valor;
+    }
+
+    get data(){
+
+        return this._data;
+    }
+
+    get quantidade(){
+
+        return this._quantidade;
+
+    }
+
+    get valor(){
+
+        return this._valor;
+    }
+}
